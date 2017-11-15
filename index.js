6134a0caa8b5131d7e187493a65cff6057fce23a
Gaël <gael@hungryup.co>
2017-11-15 14:37:21 +0100

    ✨ Location as Proxy stack

--- index.js
+++ index.js
@@ -12,22 +12,44 @@ module.exports = class LocationRouter extends AbstractRouter {
   }
 
   async run(req) {
+    const locationStack = [];
     if (req.entities && req.entities.location) {
-      const location = req.entities.location.filter(loc => loc.confidence > this.config.min_confidence);
-      const address = location.map(l => l.value).join(', ');
+      const rawLocation = req.entities.location.filter(loc => loc.confidence > this.config.min_confidence);
+      const address = rawLocation.map(l => l.value).join(', ');
       const { json: { results: [geoResults] } } = await this.googleMapsClient.geocode({
         language: 'fr_FR',
         region: 'FR',
         address,
       }).asPromise();
-      const data = geoResults;
-      req.location = {
-        full: data.formatted_address,
+
+      const location = {
+        full: geoResults.formatted_address,
         original: address,
-        latitude: data.geometry.location.lat,
-        longitude: data.geometry.location.lng,
+        latitude: geoResults.geometry.location.lat,
+        longitude: geoResults.geometry.location.lng,
       };
+      locationStack.unshift(location);
+      if (req.memory && req.memoryContext) {
+        const path = req.memoryContext.path;
+        if (path > 0) {
+          await req.memory.memorize(path[path.length - 1], { location });
+        }
+      }
     }
+    if (req.memory && req.memory.location) {
+      locationStack.unshift(req.memory.location);
+    }
+
+    req.location = new Proxy(locationStack, {
+      get: (list, name) => {
+        for (const item of list) {
+          if (item[name]) {
+            return item[name];
+          }
+        }
+        return;
+      }
+    });
   }
 
   use(fn) {
