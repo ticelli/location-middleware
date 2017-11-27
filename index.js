76147f8fb945ad3d10f6944519e7c2a1ac6c9314
Gaël <gael@hungryup.co>
2017-11-27 22:16:11 +0100

    ♻️ Refactor to fresh new API

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ module.exports = class LocationRouter extends AbstractRouter {
     });
   }
 
-  async run(req) {
+  async run(train) {
     const locationStack = [];
-    if (req.entities && req.entities.location) {
-      const rawLocation = req.entities.location.filter(loc => loc.confidence > this.config.min_confidence);
+    if (train.entities && train.entities.location) {
+      const rawLocation = train.entities.location.filter(loc => loc.confidence > this.config.min_confidence);
       const address = rawLocation.map(l => l.value).join(', ');
       const { json: { results: [geoResults] } } = await this.googleMapsClient.geocode({
         language: 'fr_FR',
@@ -29,30 +29,26 @@ module.exports = class LocationRouter extends AbstractRouter {
         longitude: geoResults.geometry.location.lng,
       };
       locationStack.unshift(location);
-      if (req.memory && req.memoryContext) {
-        const path = req.memoryContext.path;
+      if (train.memory && train.memoryContext) {
+        const { path } = train.memoryContext;
         if (path.length > 0) {
-          await req.memory.memorize(path[path.length - 1], { location });
+          await train.memory.memorize(path[path.length - 1], { location });
         }
       }
     }
-    if (req.memory && req.memory.get('location')) {
-      locationStack.unshift(req.memory.get('location'));
+    if (train.memory && train.memory.get('location')) {
+      locationStack.unshift(train.memory.get('location'));
     }
 
-    req.location = new Proxy(locationStack, {
+    train.location = new Proxy(locationStack, {
       get: (list, name) => {
         for (const item of list) {
           if (item[name]) {
             return item[name];
           }
         }
-        return;
-      }
+        return null;
+      },
     });
-  }
-
-  use(fn) {
-    throw new Error('You cannot use something on location router, excepted to be used only as middleware')
   }
 };
