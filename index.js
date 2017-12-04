ad22c9de3ec31b8e97602db177a0e01d96ea1b24
Gaël <gael@hungryup.co>
2017-12-04 14:44:55 +0100

    Plug to smarter arango memory

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ module.exports = class LocationRouter extends AbstractRouter {
   }
 
   async run(train) {
-    const locationStack = [];
     if (train.entities && train.entities.location) {
       const rawLocation = train.entities.location.filter(loc => loc.confidence > this.config.min_confidence);
       const address = rawLocation.map(l => l.value).join(', ');
@@ -28,27 +27,14 @@ module.exports = class LocationRouter extends AbstractRouter {
         latitude: geoResults.geometry.location.lat,
         longitude: geoResults.geometry.location.lng,
       };
-      locationStack.unshift(location);
-      if (train.memory && train.memoryContext) {
-        const { path } = train.memoryContext;
-        if (path.length > 0) {
-          await train.memory.memorize(path[path.length - 1], { location });
-        }
+      train.location = location;
+      if (train.memory) {
+        await train.memory.set('location', location);
+      }
+    } else {
+      if (train.memory) {
+        train.location = await train.memory.get('location');
       }
     }
-    if (train.memory && train.memory.get('location')) {
-      locationStack.unshift(train.memory.get('location'));
-    }
-
-    train.location = new Proxy(locationStack, {
-      get: (list, name) => {
-        for (const item of list) {
-          if (item[name]) {
-            return item[name];
-          }
-        }
-        return null;
-      },
-    });
   }
 };
